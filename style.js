f7f78c83396b675f2fb866f819fa0b580a18b5b6
dohabristy <labornodb@gmail.com>
2023-11-03 16:25:19 -0400

    js

---
+++ style.js
@@ -0,0 +1,32 @@
+let hed = document.getElementsByTagName("h1")
+
+let arr = Array.from(hed)
+
+// console.log(arr.length);
+
+//arr.map(item=>{
+//    console.log(item)
+//})
+
+// for(let i=0; i<=arr.length; i++){
+//     console.log(`${arr[i].innerHTML} cit`);
+// }
+
+
+// password hide to text
+// let input = document.querySelector(".input"
+// let btn = document.querySelector(".btn")
+
+
+// btn.addEventListener("click",function(){
+//     if(input.type =="password" ){
+//         input.type = "text"
+//     }else{
+//         input.type= "password"
+//     }
+// })
+
+
+
+
+
